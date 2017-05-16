abce000c87839c67edae84df7edfa64a4936e9a3
Wilmar Fuquen <wilmarfn@hotmail.com>
2017-05-15 23:44:36 -0500

    T100: Correccion nombres de doctores y procedimientos, correccion de fechas, adición de modals para Save y Delete

--- WebServer/dental1/sheduler/static/source/procedure.js
+++ WebServer/dental1/sheduler/static/source/procedure.js
@@ -1,24 +1,28 @@
 var toolProcedureJs= {
 	functionResponse: null,
-	loadProcedures: function(schedule){
+	loadProcedures: function(procedureSelected){
         toolProcedureJs.functionResponse = toolProcedureJs.responseProcedures;
-        toolProcedureJs.requestRest('/sheduler/procedures');
+        toolProcedureJs.requestRest('/sheduler/procedures',procedureSelected);
     },
-	responseProcedures: function(data){
+	responseProcedures: function(data, procedureSelected){
 		var options;
         for (var i = 0; i < data['length']; i++) {
             options += "<option value='"+data[i]['id']+"'>"+data[i]['name']+"</option>";
         }
         $('#listProcedures').append(options);
+        if(procedureSelected!=null)
+        {
+            $('#listProcedures').val(procedureSelected);
+        }
         $('#listProcedures').selectpicker('refresh');
     },
-	requestRest: function(urlRest) {
+	requestRest: function(urlRest, procedureSelected) {
         $.ajax({
             type: 'GET',
             dataType: 'json',
             url: urlRest,
             success: function(data) {
-                toolProcedureJs.functionResponse(data);
+                toolProcedureJs.functionResponse(data, procedureSelected);
             },
             error: function(xhr, ajaxOptions, thrownError) {
                 alert("Error en la respuesta JSON");
